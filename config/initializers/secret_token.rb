22db1365f96a5b00eff1e0f3f3d50b2d1a857c9e
Steph <nivot.stephane@hotmail.fr>
2012-12-07 10:07:28 +0100

    debut tp11

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TP2::Application.config.secret_token = 'edc9e6e75868754555510a0f271a82b7e97853032deea6fdfea7ec9da177ae5a5c3ac7a22196558fafdc22f8188088c762b34326937ee5120d7c45d07929244e'
